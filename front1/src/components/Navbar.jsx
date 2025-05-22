a6ed85a523216a0d1a128f78f90daa6a64ac24ec
Alanjosh457 <alanjoshua515-email@gmail.com>
2025-05-22 14:15:39 +0530

    last commit

--- front1/src/components/Navbar.jsx
+++ front1/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
+
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <h1 className={styles.logo}>Cyberpunk Meme</h1>
       <div>
-        <a href="/">Home</a> | <a href="/leaderboard">Leaderboard</a> | <a href="/create">Create Meme</a>
+        
+       <Link to="/">Home</Link> | <Link to="/leaderboard">Leaderboard</Link> | <Link to="/create">Create Meme</Link>
+
       </div>
     </nav>
   );
